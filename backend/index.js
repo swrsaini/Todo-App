8ea31155c98a4c312286899b9175fc4821842a92
Shubham Saini <Shubhamsaini98465@gmail.com>
2024-12-31 15:58:00 +0530

    added zod validation

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import { json } from "express";
 import express from express
+import { createTodo, updateTodo } from "./types";
 
 const app = express();
 
@@ -9,12 +10,25 @@ app.get('/todos', (req,res) => {
     res.send('todos')
 })
 
-app.post('/todos', (req,res) => {
+app.post('/todo', (req,res) => {
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload)
 
+    if(!parsedPayload.success){
+        res.status(411).json({msg: "you sent the wrong inputs"})
+        return;
+    }
 })
 
 app.put('/completed', (req,res) => {
-    
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+
+    if(!parsedPayload.success){
+        res.status(411).json({msg: "wrong inputs"})
+        return;
+    }
+
 })
 
 app.listen(3000);
