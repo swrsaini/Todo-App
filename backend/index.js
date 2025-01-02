16bcc1ebe9d292e88e25fe12d9190b05a4573fd2
Shubham Saini <Shubhamsaini98465@gmail.com>
2025-01-02 14:14:21 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(cors());
 
 app.use(express.json());
 
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,10 +1,21 @@
 export function Todos({todos}){
     return <div>
         {todos.map((e)=>{
-            return <div>
-                <h1>{e.title}</h1>
+            return <div key={e._id}>
+                <h2>{e.title}</h2>
                 <p>{e.description}</p>
-                <button>{e.completed == true ? "Completed" : "Mark as Complete"}</button>
+                
+                <button onClick={async ()=>{
+                    await fetch("http://localhost:3000/completed", {
+                        method: 'PUT',
+                        body: JSON.stringify({
+                            id: e._id
+                        }),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                }}>{e.completed == true ? "Completed" : "Mark as Complete"}</button>
             </div>
         })}
     </div>
