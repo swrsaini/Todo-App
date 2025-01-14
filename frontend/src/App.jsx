61d2907b3b7aedebc92e218fcf7f39e167a775db
Shubham Saini <Shubhamsaini98465@gmail.com>
2025-01-14 23:45:10 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,19 @@ import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
+
 function App() {
   
   const [count,setCount] = useState(0)
 
   return (
     <>
+    
     <div className='mb-4'>
       <h1>To Do App</h1>
     </div>
       <CreateTodo count={count} setCount={setCount} />
+
       <div className='flex justify-center items-center w-full'>
       <Todos count={count} setCount={setCount}></Todos>
       </div>
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,5 @@
+export function Search(){
+    return <div>
+        <input className="py-2 px-4 text-sm font-medium m-1" type="text" placeholder="search" />
+    </div>
+}
